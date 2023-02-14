5a94221a28f269abdd536ca0337e7114d5580e24
michelletaing <mleetaing@gmail.com>
2023-02-13 23:26:22 -0500

    added separate routing for closet component and set route parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ClosetComponent } from './closet/closet.component';
-import { ItemsComponent } from './closet/items/items.component';
+// import { ItemsComponent } from './closet/items/items.component';
 
 const routes: Routes = [
-  {path: 'closet', component: ClosetComponent},
+  {path: 'closet', component: ClosetComponent}
+  /*
   {path: 'closet', children: [
     {path: 'jeans', component: ItemsComponent} // TO DO: MAKE PATH CUSTOM TO WHATEVER IS CLICKED
   ]}
+  */
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ClosetComponent } from './closet/closet.component';
+import { ItemsComponent } from './closet/items/items.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
@@ -14,7 +14,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { ItemsComponent } from './closet/items/items.component';
+
+import { ClosetModule } from './closet/closet.module';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,6 @@ import { ItemsComponent } from './closet/items/items.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
@@ -33,6 +33,8 @@ import { ItemsComponent } from './closet/items/items.component';
     MatButtonModule,
     MatDividerModule,
     MatGridListModule,
+    ClosetModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

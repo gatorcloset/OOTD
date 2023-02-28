e178e51cfd71dbb5418c348028c8049a241c1efe
michelletaing <mleetaing@gmail.com>
2023-02-28 17:02:57 -0500

    added testing for items and signup page

---
+++ cypress/e2e/item.cy.ts
@@ -0,0 +1,14 @@
+import { CATEGORIES } from "src/app/mock-data/mock-categories"
+
+describe('Closet Item Page', () => {
+  beforeEach(() => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:4200/closet')
+  })
+  
+  it('on the jeans page, only clothing under the jeans category is displayed', () => {
+    cy.visit('http://localhost:4200/closet/jeans')
+    cy.get('mat-grid-tile')
+    cy.wrap({ category: CATEGORIES[0].name }).its('category').should('eq', 'Jeans')
+  })
+})
---
+++ cypress/e2e/signup.cy.ts
@@ -0,0 +1,17 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.viewport(1366, 768)
+    cy.visit('http://localhost:4200/signup')
+  })
+
+  it ('creates go mod file', () => {
+    // cy.exec('cd backend/go/src/github.com/user && go build && ./user')
+    // cy.exec('go build')
+    // cy.exec('./user')
+
+  })
+
+  it('passes', () => {
+    cy.visit('http://localhost:4200/signup')
+  })
+})

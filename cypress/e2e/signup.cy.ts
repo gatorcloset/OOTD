887cbeb2d6678d40889ca475a37ab0a55745b62d
michelletaing <mleetaing@gmail.com>
2023-02-28 19:49:39 -0500

    tested post function and login functionality

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -2,18 +2,25 @@ describe('template spec', () => {
   beforeEach(() => {
     cy.viewport(1366, 768)
     cy.visit('http://localhost:4200/signup')
+
+    cy.request('POST', 'http://localhost:9000/users', { 
+      firstname: 'Michelle',
+      lastname: 'Taing',
+      username: 'michelle.taing', 
+      password: 'hellothere123'
+    })
+      .its('body')
+      .as('currentUser')
   })
 
-  it ('creates go mod file', () => {
-    // cy.exec('cd backend/go/src/github.com/user && go build && ./user')
-    // cy.exec('go build')
-    // cy.exec('./user')
+  it ('fill out form and enter', function() {
+    const { firstname, lastname, username, password} = this['currentUser'];
+    cy.get('input[id=firstname]').type(firstname)
+    cy.get('input[id=lastname]').type(lastname)
+    cy.get('input[id=email]').type('mleetaing@gmail.com')
+    cy.get('input[id=username]').type(username)
+    cy.get('input[id=password]').type(password)
+    cy.get('.login-button').first().click('center')
 
   })
-
-  it('passes', () => {
-    cy.visit('http://localhost:4200/signup')
-  })
-
-  // delete this comment
 })

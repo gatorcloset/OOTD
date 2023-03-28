bb2446072f0a5418728d331e8c2968f7c6c5416e
Aneesha Acharya <aneeshaacharya@Aneeshas-MacBook-Pro.local>
2023-03-28 17:29:01 -0400

    completely functionaly sliding through tops

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -15,10 +15,31 @@ export class CarouselComponent implements OnInit{
 
   @Input() images: carouselImage[] = []
   @Input() indicators = true;
+  @Input() controls = true;
 
   selectedIndex = 0;
 
   ngOnInit():void {
+  }
 
+  // sets index of image on dot click
+  selectImage(index: number): void {
+    this.selectedIndex = index;
+  }
+
+  onPrevClick(): void {
+    if(this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+
+  onNextClick(): void {
+    if(this.selectedIndex === this.images.length-1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
   }
 }

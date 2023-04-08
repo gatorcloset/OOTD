b2d2018feefd084f03973b371eddfe0140cc4add
michelletaing <mleetaing@gmail.com>
2023-04-08 15:51:57 -0400

    added authenticated user logic

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { tap, catchError } from 'rxjs';
 import { User } from '../mock-data/user';
 import { LoginRequest } from '../mock-data/user';
 
@@ -9,34 +10,19 @@ import { LoginRequest } from '../mock-data/user';
 })
 export class UserService {
   private apiURL = 'http://localhost:9000/users';
-  authUser: User = <User>{};
+  
+  // Authenticated user information
+  authenticated = false;
+  authUser?: User = <User>{};
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
   
-  /*
-  session = {
-    headers: new HttpHeaders({ 
-      'Content-Type':  'application/json'
-    }),
-    withCredentials: true
-  };
-  */
-
   constructor(private http: HttpClient) { }
 
-  setAuthUser(user: User) {
-    this.authUser = user;
-  }
-
-  getAuthUser(): User {
-    return this.authUser;
-  }
-
   getUser(): Observable<User> {
-    const userID = sessionStorage.getItem('userID'); // THIS IS CURRENTLY NULL
-    const url = `${this.apiURL}/${userID}`;
+    const url = `${this.apiURL}/${this.authUser?.ID}`;
     return this.http.get<User>(url);
   }
 
@@ -48,6 +34,6 @@ export class UserService {
   loginUser(user: LoginRequest): Observable<User> {
     const url = "http://localhost:9000/login";
     const options = { ...this.httpOptions, withCredentials: true };
-    return this.http.post<User>(url, user, options);
+    return this.http.post<User>(url, user, options)
   }
 }

314233e2f6a164c6522a71eeb7efa0501071b4c1
Aneesha Acharya <aneeshaacharya@Aneeshas-MacBook-Pro.local>
2023-04-17 16:39:34 -0400

    format outfits display

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,10 +12,11 @@ import { AuthGuard } from './services/auth-guard.service';
 import { ItemsComponent } from './items/items.component';
 import { OutfitsComponent } from './outfits/outfits.component';
 
+
 const routes: Routes = [
   { path: 'closet', component: ClosetComponent, canActivate: [AuthGuard] },
   { path: 'closet/:name', component: ItemsComponent, canActivate: [AuthGuard] },
-  { path: 'closet/outfits', component: OutfitsComponent},
+  { path: 'closet/outfits', component: OutfitsComponent, canActivate: [AuthGuard]},
   { path: '', component: EntranceComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
@@ -24,6 +25,21 @@ const routes: Routes = [
   { path: 'add', component: NewItemComponent, canActivate: [AuthGuard] },
   { path: 'builder', component: CarouselComponent, canActivate: [AuthGuard] }
 ];
+
+/*
+const routes: Routes = [
+  { path: 'closet', component: ClosetComponent},
+  { path: 'closet/:name', component: ItemsComponent},
+  { path: 'outfits', component: OutfitsComponent},
+  { path: '', component: EntranceComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'user', component: UserComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'add', component: NewItemComponent},
+  { path: 'builder', component: CarouselComponent}
+];
+*/
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

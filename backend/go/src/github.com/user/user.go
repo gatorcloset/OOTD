31b49862a7ae621e27a04bacca6b599ba6c65f8e
michelletaing <mleetaing@gmail.com>
2023-04-17 14:45:14 -0400

    added json field to outfit name

--- backend/go/src/github.com/user/user.go
+++ backend/go/src/github.com/user/user.go
@@ -53,7 +53,7 @@ type ItemTag struct {
 
 type Outfit struct {
 	gorm.Model
-	name		  string
+	Name		  string `json:"Name"`
 	Tops          Item `gorm:"foreignKey:TopID"`
 	TopID         uint
 	Bottoms       Item `gorm:"foreignKey:BottomID"`

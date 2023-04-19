1295cf406b2b027635e7c96f55d3b05aac6e7466
michelletaing <mleetaing@gmail.com>
2023-04-19 11:56:56 -0400

    updated cypress testing to fix minor bugs

--- cypress/e2e/edit.cy.ts
+++ cypress/e2e/edit.cy.ts
@@ -16,8 +16,8 @@ describe('Edit and delete clothing item', () => {
 
   it('Edit clothing item image, name, and category', () => {
     // Select clothing item
-    cy.get('mat-grid-tile').first().click()
-    cy.get('mat-grid-tile').last().click()
+    cy.get('mat-grid-tile').get('#Bottoms').click()
+    cy.get('mat-grid-tile').contains('Denim shorts').click()
 
     // Change image
     cy.get('input[type="file"]').selectFile('cypress/fixtures/mock-shirt.jpg')
@@ -26,18 +26,19 @@ describe('Edit and delete clothing item', () => {
     cy.get('#name').clear().type('Button up')
 
     // Change category
-    cy.get('#categories').click();
-    cy.contains('Top').click();
+    cy.get('#categories').contains('Top').click()
     
-    // cy.get('#save-button').click()
+    cy.get('#save-button').click()
   })
 
-  it('Check edited item', () => {
+  it('Check edited item and delete', () => {
     // Locate item just edited
     cy.contains('mat-card', 'Tops').click()
     cy.contains('mat-card', 'Button up').click()
 
     cy.get('#categories').contains('Top')
+
+    cy.get('#delete-button').click();
   })
 
 

0c259c728387f82cc1c832c48f2ec391b5a92389
Aneesha Acharya <aneeshaacharya@Aneeshas-MacBook-Pro.local>
2023-04-19 04:38:57 -0400

    add icons for closet categories

--- src/app/mock-data/default-categories.ts
+++ src/app/mock-data/default-categories.ts
@@ -2,9 +2,9 @@ import { Category } from './category';
 
 
 export const CATEGORIES: Category[] = [
-    { name: 'Bottoms', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081' },
-    { name: 'Tops', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081' },
-    { name: 'Shoes', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'},
-    { name: 'Accessories', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'},
-    { name: 'One-Pieces', image: 'https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081'}
+    { name: 'Tops', image: 'assets/closet-icons/tops-icon.svg'},
+    { name: 'Bottoms', image: 'assets/closet-icons/bottoms-icon.svg'},
+    { name: 'Shoes', image: 'assets/closet-icons/shoes-icon.svg'},
+    { name: 'Accessories', image: 'assets/closet-icons/accessories-icon.svg'},
+    { name: 'One-Pieces', image: 'assets/closet-icons/one-pieces-icon.svg'}
 ]
